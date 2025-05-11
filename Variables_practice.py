1a0c2dcc05c4f50df1a7a1812029fe559ccb45c3
Dawood Iftakhar <dawood.iftakhar87@gmail.com>
2025-05-11 11:04:30 +0500

    Add files via upload

---
+++ Variables_practice.py
@@ -0,0 +1,15 @@
+NAME="Dawood"
+AGE=37
+COURSE_NAME="Python Basics"
+# 2name="Dawood" # x Can not start with a number
+# my-name="Dawood" # x Dash not allowed in variable name
+# User Name="Dawood" # x Space not allowed in variable name
+print(NAME)
+print(AGE)
+print(COURSE_NAME)
+STUDENT_NAME="Dawood"
+STUDENT_AGE=37  
+TOTAL_MARKS=95
+print(STUDENT_NAME)
+print(STUDENT_AGE) 
+print(TOTAL_MARKS)
